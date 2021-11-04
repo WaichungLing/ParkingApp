c89a29f9fc98e05b23e3888af03821bcd529ae1d
nelson-truong <ngtruong2@g.ucla.edu>
2021-11-04 11:05:13 -0700

    Add a JoinCreate.js page - Displays a message asking user about joining / creation

---
+++ src/Pages/JoinCreate.js
@@ -0,0 +1,14 @@
+import {Button, Box, Divider, Grid, TextField, Typography } from "@mui/material";
+
+function JoinCreate() {
+
+    return (
+	<div className="JoinCreate">
+	    <Typography align="center" variant="h1">"Do you want to join an existing ApartmentParking or create a new one?"</Typography>
+	    
+	</div>
+    );
+    
+}
+
+export default JoinCreate;

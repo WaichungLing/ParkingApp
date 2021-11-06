3e9a6c3704d68b6543b1c05c2ef4748d631049f2
zw85 <zw85@cs.washington.edu>
2021-11-05 17:58:11 -0700

    modify login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,45 +1,101 @@
 import {Button, Box, Divider, Grid, TextField, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Image from "../Images/cars.png";
+import {useState} from "react";
 
 function Login() {
 
-    /*
-    * 1. Create a new .js file in src, you can create multiple .js files as components and import them in a final file
-    * 2. To test how it looks like on web, simply replace the <App /> in index.js with the .js file you want to use
-    * 3. How to import other components? As simple as the <PlainText /> in this example.
-    * 4. For styling, you can include a css file like ./App.cs, or use sx prop provided by Material UI,
-    *    see https://mui.com/system/basics/#demo
-    * 5. When push the code, try to use .gitignore to ignore node_modules directory, because there are too many files.
-    */
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main:'#60A166',
+			},
+		},
+	});
 
-    const styles = {
-        paperContainer: {
-            backgroundImage: `url(${Image})`
-        }
-    };
+	const styles = {
+		paperContainer: {
+			backgroundImage: `url(${Image})`,
+			height: "100%",
+			width: "100%",
+		},
+		text: {
+			color: "#60A166",
+			fontWeight: 600,
+			fontSize: 43,
+		},
+		inputSet: {
+			display: "flex",
+			flexDirection: "column",
+			width: "65%",
+		},
+		signupButton: {
+			backgroundColor: "#60A166",
+			fontWeight: 600,
+			fontSize: 25,
+		},
+		loginButton: {
+			color: "#60A166",
+			fontSize: 20,
+		},
+		loginGrid: {
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "center",
+		}
+	};
 
-    return (
-      <div className="Login">
+	const [UserName, setUserName] = useState('');
+	const [PhoneNumber, setPhoneNumber] = useState('');
+	const [Email, setEmail] = useState('');
+	const [Password, setPassword] = useState('');
 
-        <Grid container>
-            <Grid item xs={6} sm={6} md={6}>
-                <Box style={styles.paperContainer} sx={{height: "100%", width: "100%"}}></Box>
-            </Grid>
-            <Grid item xs={6} sm={6} md={6} container direction="column" justifyContent="center" alignItems="center">
-                <Typography style={{color: "#60A166", fontWeight: 600, fontSize: 43}} component="h1">PARKING APP</Typography>
-                <Box sx={{display: "flex", flexDirection: "column", width: "65%"}}>
-                    <TextField id="username" label="UserName" variant="outlined" margin="normal"></TextField>
-                    <TextField id="phonenumber" label="Phone #" variant="outlined" margin="normal"></TextField>
-                    <TextField id="email" label="Email" variant="outlined" margin="normal"></TextField>
-                    <TextField id="password" label="Password" variant="outlined" margin="normal"></TextField>
-                    <Button style={{backgroundColor: "#60A166", fontWeight: 600, fontSize: 25}} type="submit" variant="contained" sx={{mt: 3, mb: 2}}>SIGNUP</Button>
-                    <Divider variant="middle" />
-                    <Button style={{color: "#60A166", fontSize: 20}} type="submit" variant="text"> LOGIN</Button>
-                </Box>
-            </Grid>
-        </Grid>
-      </div>
-    );
-  }
+	return (
+		<ThemeProvider theme={theme}>
+			<Grid container>
+				<Grid item xs={6} sm={6} md={6}>
+					<Box style={styles.paperContainer}></Box>
+				</Grid>
+				<Grid item xs={6} sm={6} md={6} container style={styles.loginGrid}>
+					<Typography style={styles.text} component="h1">PARKING APP</Typography>
+					<Box style={styles.inputSet}>
+						<TextField id="username"
+											 label="UserName"
+											 value={UserName}
+											 variant="outlined"
+											 margin="normal"
+											 onChange={(e) => setUserName(e.target.value)}>
+						</TextField>
+						<TextField id="phonenumber"
+											 label="Phone #"
+											 value={PhoneNumber}
+											 variant="outlined"
+											 margin="normal"
+											 onChange={(e) => setPhoneNumber(e.target.value)}>
+						</TextField>
+						<TextField id="email"
+											 label="Email"
+											 value={Email}
+											 variant="outlined"
+											 margin="normal"
+											 onChange={(e) => setEmail(e.target.value)}>
+						</TextField>
+						<TextField id="password"
+											 label="Password"
+											 value={Password}
+											 variant="outlined"
+											 margin="normal"
+											 onChange={(e) => setPassword(e.target.value)}>
+						</TextField>
+						<Button style={styles.signupButton} type="submit" variant="contained" sx={{mt: 3, mb: 2}}>SIGNUP</Button>
+						<Divider variant="middle" />
+						<Button style={styles.loginButton} type="submit" variant="text"> LOGIN</Button>
+					</Box>
+				</Grid>
+			</Grid>
+		</ThemeProvider>
+	);
+}
 
   export default Login;

967c6e85f328a18104b2cd5476e39bd788bdede1
victorczhou <victorczhou@gmail.com>
2021-11-07 22:26:31 -0800

    added backend endpoints

---
+++ server/routes/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;		// convert id from String to ObjectID
+
+// may move to separate files later
+
+router.route("/users/:id").get(function (req, res){
+
+});
+
+router.route("/users/create").post(function (req, res){
+	
+});
+
+router.route("/users/:id").post(function (req, res){	// update
+	
+});
+
+router.route("/users/:id").delete(function (req, res){
+	
+});
+
+
+router.route("/apts/:id").get(function (req, res){
+
+});
+
+router.route("/apts/create").post(function (req, res){
+	
+});
+
+router.route("/apts/:id").post(function (req, res){	// update
+	
+});
+
+router.route("/apts/:id").delete(function (req, res){
+	
+});

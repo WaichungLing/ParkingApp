64dd522d03ed906bbc1d764118e123e58d816139
victorczhou <victorczhou@gmail.com>
2021-11-17 02:11:45 -0800

    added twilio api

--- server/models/apartment.model.js
+++ server/models/apartment.model.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
+const Twilio = require('twilio');
+console.log(process.env);
+const twilio_sid = process.env.TWILIO_SID;
+const twilio_token = process.env.TWILIO_AUTHTOKEN;
+const twilio_phone_number = process.env.TWILIO_PHONE_NUMBER;
+
 const Schema = mongoose.Schema;
 
 const apt_schema = new Schema({
@@ -7,12 +14,37 @@ const apt_schema = new Schema({
     num_lanes: { type: Number, required: true},
     num_spots: { type: Number, required: true},
     spots: {type: mongoose.Mixed, required: true},
-    movetimes: {type: Date, required: true}					// for notifcations
-})
+    movetimes: {type: Date, required: true}						// for notifcations
+});
+
+const testDate = new Date('December 17, 1995 03:24:00');		// some arbitrary time
+const testNumber = 15105168560;									// this is my number dont call me
+const testName = "Victor";										// switch to Apartment.user.name for each user
 
 apt_schema.statics.sendNotifications = function(callback){
 	const nowDate = new Date();
-	console.log("temp hold here test");
+	if (nowDate >= testDate){									// always true for testing rn
+		const client = new Twilio(twilio_sid, twilio_token);
+		const options = {
+            to: `+ ${testNumber}`,
+            from: `+ ${twilio_phone_number}`,
+            /* eslint-disable max-len */
+            body: `Hi ${testName}. Time to move your car!`,
+            /* eslint-enable max-len */
+        };
+        client.messages.create(options, function(err, response) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                console.log(`Message sent to ${testName}: ${testNumber}`);
+            }
+        });
+
+        if (callback){
+        	callback.call();
+        }
+	}
 }
 
 const Apt = mongoose.model('Apt', apt_schema);
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const scheduler = require('./scheduler');
-scheduler.start();
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
+const scheduler = require('./scheduler');
+scheduler.start();
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/app"));
